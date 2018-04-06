c374b0d3c19ab37e7e7529f8088eca40eaf8a105
BrandonAmyot <amyot.brandon@hotmail.com>
2018-04-06 16:39:29 -0400

    Fetching Scripts added
    appends it to text file.

--- CSVParser.py
+++ CSVParser.py
@@ -1,6 +1,6 @@
 import csv
 home = 'Test_HomeNetwork'
-public = 'Test_Starbucks'
+public = 'Test_Concordia'
 
 # Get cookie domain names from home network
 with open('CSV/'+home+'/profile_cookies.csv') as csvfile:
@@ -8,7 +8,7 @@ with open('CSV/'+home+'/profile_cookies.csv') as csvfile:
     homeCookies = []
     for row in reader:
         homeCookies.append(row['baseDomain'])
-    homeCookies = list(set(homeCookies))
+    homeCookies = set(homeCookies)
 
 # Get cookie domain names from public network
 with open('CSV/'+public+'/profile_cookies.csv') as csvfile:
@@ -16,15 +16,45 @@ with open('CSV/'+public+'/profile_cookies.csv') as csvfile:
     publicCookies = []
     for row in reader:
         publicCookies.append(row['baseDomain'])
-    publicCookies = list(set(publicCookies))
+    publicCookies = set(publicCookies)
 
 # Find and print cookies present on public networks that are not present on home networks
-difference = list(set(publicCookies) - set(homeCookies))
-for list in difference:
+cookieDifference = (set(publicCookies) - set(homeCookies))
+for list in cookieDifference:
     print (list)
 
 # Get 3rd party scripts
+with open('CSV/'+home+'/http_responses.csv') as csvfile:
+    reader = csv.DictReader(csvfile)
+    homeScripts = []
+    for row in reader:
+        if row['url'].endswith('.js'):
+            homeScripts.append(row['url'])
+    homeScripts = set(homeScripts)
+
+for list in homeScripts:
+    print(list)
+
+# Get 3rd party scripts
+with open('CSV/'+public+'/http_responses.csv') as csvfile:
+    reader = csv.DictReader(csvfile)
+    publicScripts = []
+    for row in reader:
+        if row['url'].endswith('.js'):
+            publicScripts.append(row['url'])
+    publicScripts = set(publicScripts)
+
+scriptDifference = (set(publicScripts) - set(homeScripts))
+for list in scriptDifference:
+    print (list)
+
+# finalResult = cookieDifference.j
 
 file = open('output.txt', 'w')
-file.write(', \n'.join(difference))
+file.write('COOKIES\n')
+file.write(', \n'.join(cookieDifference))
+file.close()
+file = open('output.txt', 'a')
+file.write('\n\n3RD PARTY SCRIPTS\n')
+file.write(', \n'.join(scriptDifference))
 file.close()
