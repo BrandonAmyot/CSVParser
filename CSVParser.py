93919b5510d5d49b6ec9da92909e8762400dbd77
BrandonAmyot <amyot.brandon@hotmail.com>
2018-04-06 16:07:13 -0400

    Begin script parser section

--- CSVParser.py
+++ CSVParser.py
@@ -1,6 +1,9 @@
 import csv
+home = 'Test_HomeNetwork'
+public = 'Test_Starbucks'
+
 # Get cookie domain names from home network
-with open('CSV/Test_HomeNetwork/profile_cookies.csv') as csvfile:
+with open('CSV/'+home+'/profile_cookies.csv') as csvfile:
     reader = csv.DictReader(csvfile)
     homeCookies = []
     for row in reader:
@@ -8,18 +11,19 @@ with open('CSV/Test_HomeNetwork/profile_cookies.csv') as csvfile:
     homeCookies = list(set(homeCookies))
 
 # Get cookie domain names from public network
-with open('CSV/Test_Starbucks/profile_cookies.csv') as csvfile:
+with open('CSV/'+public+'/profile_cookies.csv') as csvfile:
     reader = csv.DictReader(csvfile)
     publicCookies = []
     for row in reader:
         publicCookies.append(row['baseDomain'])
     publicCookies = list(set(publicCookies))
 
-
 # Find and print cookies present on public networks that are not present on home networks
 difference = list(set(publicCookies) - set(homeCookies))
 for list in difference:
     print (list)
+
+# Get 3rd party scripts
 
 file = open('output.txt', 'w')
 file.write(', \n'.join(difference))
