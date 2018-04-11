a77406fc43c9efb7dfdecc30f004ce5a33f9b164
Richard <mcccaughey@gmail.com>
2018-04-11 13:06:07 -0400

    Added some changes to total the home/public cookies and output them in a .txt file

--- CSVParser.py
+++ CSVParser.py
@@ -1,6 +1,6 @@
 import csv
 home = 'Test_HomeNetwork'
-public = 'Test_LeVietSub'
+public = 'Test_PlaceDesArts'
 
 # Get cookie domain names from home network
 with open('CSV/'+home+'/profile_cookies.csv') as csvfile:
@@ -10,6 +10,7 @@ with open('CSV/'+home+'/profile_cookies.csv') as csvfile:
         homeCookies.append(row['baseDomain'])
     homeCookies = set(homeCookies)
 
+print("Home Cookies:" + str(len(homeCookies)))
 # Get cookie domain names from public network
 with open('CSV/'+public+'/profile_cookies.csv') as csvfile:
     reader = csv.DictReader(csvfile)
@@ -18,13 +19,14 @@ with open('CSV/'+public+'/profile_cookies.csv') as csvfile:
         publicCookies.append(row['baseDomain'])
     publicCookies = set(publicCookies)
 
+homeInfo = "\nHome Cookies:" + str(len(homeCookies)) + "\nPublic Cookies:" + str(len(publicCookies)) + "\nTotal Cookies: " + (str(len(homeCookies) + len(publicCookies)) + '\n')
+
 # Find and print cookies present on public networks that are not present on home networks
 cookieDifference = (set(publicCookies) - set(homeCookies))
 for list in cookieDifference:
     print (list)
 
-cookies_total = "\nCookies Total: " + str(len(cookieDifference))
-print(cookies_total)
+cookies_total_difference = "Cookies Difference: " + str(len(cookieDifference))
 # Get 3rd party scripts
 with open('CSV/'+home+'/http_responses.csv') as csvfile:
     reader = csv.DictReader(csvfile)
@@ -44,21 +46,26 @@ with open('CSV/'+public+'/http_responses.csv') as csvfile:
             publicScripts.append(row['url'])
     publicScripts = set(publicScripts)
 
+publicInfo = "\nHome Scripts: " + str(len(homeScripts)) + "\nPublic Scripts: " + str(len(publicScripts)) +"\nTotal Scripts: " + (str(len(homeScripts) + len(publicScripts)) +'\n')
+
 scriptDifference = (set(publicScripts) - set(homeScripts))
 for list in scriptDifference:
     print (list)
 
-scripts_total = "\nScripts Total: " + str(len(scriptDifference))
-print(scripts_total)
+scripts_difference_total = "Scripts Difference: " + str(len(scriptDifference))
 # finalResult = cookieDifference.j
 
 file = open('output'+ public + '.txt', 'w')
 file.write('COOKIES\n')
 file.write(', \n'.join(cookieDifference))
-file.write(cookies_total)
+file.write('\n\n***COOKIES INFO***')
+file.write(homeInfo)
+file.write(cookies_total_difference)
 file.close()
 file = open('output'+ public + '.txt', 'a')
 file.write('\n\n3RD PARTY SCRIPTS\n')
 file.write(', \n'.join(scriptDifference))
-file.write(scripts_total)
+file.write('\n\n***SCRIPTS INFO***')
+file.write(publicInfo)
+file.write(scripts_difference_total)
 file.close()
